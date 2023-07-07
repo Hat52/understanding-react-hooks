346bef2a74771d15026b7936ccb4e893e2c6b076
Hat52 <tanolihamzaali@gmail.com>
2023-07-07 18:10:00 +0500

    change:componenets data structure

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,27 @@ import { Card } from './components';
 import { Grid } from '@mui/material';
 
 const hooksCollection = [
-	'useState',
-	'useEffect',
-	'useContext',
-	'useRef',
-	'useMemo',
-	'useCallback',
-	'useReducer',
-	'useTransition',
-	'useDeferredValue',
-	'useLayoutEffect',
-	'useDebugValue',
-	'useImperativeHandle',
-	'useID',
-	'Custom Hooks'
+	{ title: 'useState', component: <>Use State</> },
+	{ title: 'useEffect', component: <>Use Effect</> },
+	{ title: 'useContext', component: <UseContextHook /> },
+	{ title: 'useRef', component: <>Use Ref</> },
+	{ title: 'useMemo', component: <>Use Memo</> },
+	{ title: 'useCallback', component: <>Use Callback</> },
+	{ title: 'useReducer', component: <>Use Reducer</> },
+	{ title: 'useTransition', component: <>Use Transition</> },
+	{ title: 'useDeferredValue', component: <>Use DeferredValue</> },
+	{ title: 'useLayoutEffect', component: <>Use LayoutEffect</> },
+	{ title: 'useDebugValue', component: <>Use Debug Value</> },
+	{ title: 'useImperativeHandle', component: <>Use ImperativeHandle</> },
+	{ title: 'useID', component: <>Use id</> },
+	{ title: 'Custom Hooks', component: <>Custom Hooks</> }
 ];
 
 function App() {
 	return (
 		<Box style={{ padding: 80 }}>
 			<Grid container spacing={2} justifyContent="center">
-				{hooksCollection.map((title) => (
+				{hooksCollection.map(({ title }) => (
 					<Grid item xs={12} md={3}>
 						<Card title={title} />
 					</Grid>
